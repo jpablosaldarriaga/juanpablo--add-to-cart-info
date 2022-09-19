086847b3f73601f3fdbe9dafdfffd98a501b27e7
@pablo.saldarriaga.coxti <pablo.saldarriaga@coxti.net>
2022-09-19 17:04:32 -0500

    feature: add to cart info

---
+++ react/components/ButtonGroup.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ButtonGroup = () => {
+  return (
+    <>
+      <div>
+        <a href="/">CHECK OUT</a>
+      </div>
+      <div>
+        <button>CONTINUA COMPRANDO</button>
+        <a href="/">VER CARRITO</a>
+      </div>
+    </>
+  )
+}
+
+export default ButtonGroup
